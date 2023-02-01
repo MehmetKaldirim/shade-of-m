125e40d4ddbd6a5690a21ddf581ff35ae36aeabb
Mehmet <mathdenizi@gmail.com>
2023-02-01 16:06:33 +0100

    PostAuthor and TimeAgo added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
