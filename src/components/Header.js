f644af273eefb875429ea322a3d3b549d7255272
Mehmet <mathdenizi@gmail.com>
2023-02-02 21:59:44 +0100

    Code simplified, optimized; using  createntityadap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCount, getCount } from "../features/posts/postsSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(getCount);
+
   return (
     <header className="Header">
-      <h1>Shade Of M</h1>
+      <h1>Redux Blog</h1>
       <nav>
         <ul>
           <li>
@@ -12,7 +17,11 @@ const Header = () => {
           <li>
             <Link to="post">Post</Link>
           </li>
+          <li>
+            <Link to="user">Users</Link>
+          </li>
         </ul>
+        <button onClick={() => dispatch(increaseCount())}>{count}</button>
       </nav>
     </header>
   );
